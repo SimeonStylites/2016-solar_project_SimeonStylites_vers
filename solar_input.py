841ef264af45f5e873d106911373e668d1abb2d5
SimeonStylites <klimachkovdmitry@gmail.com>
2023-11-01 17:31:04 +0300

    Написана формула для силы. Написано перемещение объектов. Считывание из файла доделано. Работает теперь

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -40,11 +44,21 @@ def parse_star_parameters(line, star):
 
     Параметры:
 
-    **line** — строка с описание звезды.
+    **line** — строка с описанием звезды.
     **star** — объект звезды.
     """
+    parameters = line.split()
+    if parameters[0]=="Star":
+        star.R = int(parameters[1])
+        star.color = parameters[2]
+        star.m = float(parameters[3])
+        star.x = float(parameters[4])
+        star.y = float(parameters[5])
+        star.Vx = float(parameters[6])
+        star.Vy = float(parameters[7])
+    else:
+        pass
 
-    pass  # FIXME: not done yet
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
@@ -61,7 +75,17 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
+    parameters = line.split()
+    if parameters[0]=="Planet":
+        planet.R = int(parameters[1])
+        planet.color = parameters[2]
+        planet.m = float(parameters[3])
+        planet.x = float(parameters[4])
+        planet.y = float(parameters[5])
+        planet.Vx = float(parameters[6])
+        planet.Vy = float(parameters[7])
+    else:
+        pass
 
 
 def write_space_objects_data_to_file(output_filename, space_objects):
